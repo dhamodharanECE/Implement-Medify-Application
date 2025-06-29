3504cb64ace93fd42988aa9adbaa72c004936ff5
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-06-29 10:47:39 +0530

    Implementation Medify Project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,37 @@
-import { Outlet } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
-import DownloadApp from "./Components/Server/TopSections/DownloadApp/DowmloadApp";
-import Footer from "./Components/Server/Footer/Footer";
-
+import React , { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SearchResult from "./pages/SearchResult/SearchResults";
+import MyBookings from "./pages/Bookings/MyBookings";
+import { AppContext } from "./Context/AppContext";
+import Vission from './components/Vission/Vission'
+import Navbar from './components/Navbar/Navbar';
+import Access from "./components/Accessibility/Access";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div>
-      <CssBaseline />
-      <Outlet />
-      <DownloadApp />
-      <Footer />
-    </div>
-  );
+     const [states,setStates] = useState([]);
+     const [cities,setCities] = useState([]);
+     const [centers,setCenters] = useState([]);
+     const [selectedState , setSelectedState] = useState('');
+     const [selectedCity, setSelectedCity] = useState(''); 
+     const [bookingDetails, setBookingDetails] = React.useState(null);
+
+     console.log('selected state:', selectedState);
+     console.log('selected city:', selectedCity);
+    return (
+        <AppContext.Provider value={{ states, setStates, cities, setCities, centers, setCenters, selectedState, setSelectedState, selectedCity, setSelectedCity , bookingDetails, setBookingDetails }}>
+            <Vission/>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/SearchResult" element={<SearchResult />} />
+                <Route path="/my-bookings" element={<MyBookings />} />
+            </Routes>
+            <Access/>
+            <Footer/>
+        </AppContext.Provider>
+    )
 }
 
 export default App;
